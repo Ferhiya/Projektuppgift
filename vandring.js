314bafa3700163d500cee84123db420b1028c2eb
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2022-05-10 16:39:46 +0200

    vatten aktivter och vandrings sidorna

--- vandring.js
+++ vandring.js
@@ -4,16 +4,23 @@ var flickrImgElem;		// Referens till element där bilderna ska visas
 var skicka; //knapp för att lägga till kommentar
 var valt; //refrens till div elemetet där info om vandrignsledet ska visas
 var knappar; //refens till tryckt knapp
+var display;
+var ledinfo;
 	
 // Initiering av globala variabler och händelsehanterare
 function init() {
 	flickrImgElem = document.getElementById("flickrImg");
 	valt=document.getElementById("valt");
 	knappar=document.getElementsByClassName("knappar");
+    display=document.getElementById("vandringslederna");
 
 	for (let i = 0; i < knappar.length; i++) {
 		knappar[i].addEventListener("click", requestData);
+        knappar[i].addEventListener("click", showledinfo);
+        
 	}
+
+
 	
 
 	skicka= document.getElementById("skicka");
@@ -24,7 +31,9 @@ function init() {
 		var text = document.createTextNode(kommenteraValue);
 		p.appendChild(text);
 		document.getElementById("unordered").appendChild(p);
+        document.getElementById("unordered").appendChild(p).style.border="2px solid black";
 	});
+  
 
 	//requestData();
 	requestvader();
@@ -37,69 +46,80 @@ window.addEventListener("load",init);
 
 //-------------------------------------------------------------------------------------------------
 
-
 // Gör ett Ajax-anrop för att läsa in begärd fil
 function requestData(e) { 
-	let id = e.target.id;
+    let id = e.target.id;
 	let request = new XMLHttpRequest(); // Object för Ajax-anropet
-	request.open("GET","vandring" + ".json",true);
+	request.open("GET","vandring" + id + ".json",true);
 	request.send(null); // Skicka begäran till servern
 	request.onreadystatechange = function () { //funktion för att avläsa kommunikation i filenhämtningen
 		if (request.readyState == 4) //staus 4=kommunikation klar
-			if (request.status == 200) getData(request.responseText, id); //Status ok=filen finns. responseText=för att man hämtar en JSON fil.
+			if (request.status == 200) getData(request.responseText); //Status ok=filen finns. responseText=för att man hämtar en JSON fil.
 			else valt.innerHTML = "Den begärda filen finns inte."; //error msg när begärd fil inte finns
 	};
+
+    
 } 
 
-/*
-// Avläs tryckt knapp för att visa datan i json filen som hör ihop med den knappen.
-function selecthike() {
-	let knapp=this.id; //hämtar nummeret frå tryckt knapp , för att senare använda det för att hämta information till vald led .
-	requestData(knapp); //anroppar functionen som läser in xml-filen
-	console.log(knapp);
-	
-} // End selectSubject
-*/
-
 // Tolkar koden och skriv ut den på önskad form
-function getData(JSONtext,id) {
+function getData(JSONtext) {
 	let vandring = JSON.parse(JSONtext).vandring; //hämtar arryen med vandringsledernas data.
 
 	let HTMLcode = ""; //tom html sträng för utskriften av innehållet i JSON 
-	for (let i = 0; i < vandring.length; i++) {
-		let id = JSON.parse(JSONtext).vandring.id;
-		console.log(id);
-		 if (vandring[i].id === id) {
-			console.log(vandring[i].id);
-			// Referenser till olika egenskaper i vandrings objektet i JSON
-			HTMLcode += "<p><b>led:</b> " + vandring[i].led + "</p>" + //lägger in namnet på ledet i html strängen
-			"<p><b>längd:</b> " + vandring[i].längd + "</p>" + //lägger in längden på ledet i html strängen
-			"<p><b>handikapsanpassat:</b> " + vandring[i].handikapsanpassat + "</p>" + //lägger in info om handikapsanpassning i ledet i html strängen
-			"<p><b>beskrivning:</b> " + vandring[i].beskvivning + "</p>" + //lägger in en kort beskrivning om ledet i html strängen
-			"<p><b>svårighetsnivå:</b> " + vandring[i].svårighetsnivå + "</p>" + //lägger in ledets svårighetsnivå i html strängen
-			"<p><b>parkering:</b> " + vandring[i].parkering + "</p>"+ //lägger in info om parkering i html strängen
-			"<hr>"
-			;
-			valt.innerHTML = HTMLcode; //utskrift av datan i JSON filen
-		}
-	
-	valt.innerHTML = HTMLcode; //utskrift av datan i JSON filen
-		
-	}
 
+	HTMLcode += 
+	"<h1><b></b> " + vandring.stad + "</h1>" +"<hr>";
+    
+    
+	for (let i = 0; i < vandring.length; i++) {
+			// Referenser till olika egenskaper i vandrings objektet i JSON
+			HTMLcode += 
+            "<h2><b></b> " + vandring[i].led + "</h2>" + //lägger in namnet på ledet i html strängen
+			//console.log(vandring[i].imgurl);
+			//"<a herf='" + vandring[i].url+ "'></a>" + //lägger in en kort beskrivning om ledet i html strängen
+			
+			//"<a href=´Läs mer här" + vandring[i].url  + "</a>"+
+			//'<a href="Läs mer>'  + vandring[i].url + '</a>'+
+			"<li><b>längd:</b> " + vandring[i].längd + "</li>" + //lägger in längden på ledet i html strängen
+			"<li><b>handikapsanpassat:</b> " + vandring[i].handikapsanpassat + "</li>" + //lägger in info om handikapsanpassning i ledet i html strängen
+			"<li><b>svårighetsnivå:</b> " + vandring[i].svårighetsnivå + "</li>" + //lägger in ledets svårighetsnivå i html strängen
+			"<li><b>parkering:</b> " + vandring[i].parkering + "</li>"+ //lägger in info om parkering i html strängen
+			"<img src='"+vandring[i].image.url+"'></img>"+
+			"<p><b></b> " + vandring[i].beskrivning + "</p>" + //lägger in en kort beskrivning om ledet i html strängen
+			"<a href='"+vandring[i].link.linkurl+"' target=_blank>Läs mer</a>"+
+			"<hr>"
+			
 	
+			//valt.innerHTML = HTMLcode; //utskrift av datan i JSON filen
+		}
+
+
+	    valt.innerHTML = HTMLcode; //utskrift av datan i JSON filen
+		//li.style.display=" flex";
+        valt.style.fontSize = "150%";
+        valt.style.marginBottom = "5%";
+        document.getElementById("kommentera").style.width= "350px";
+        document.getElementById("kommentera").style.height= "150px";
+        document.getElementById("skicka").style.width= "100px";
+        //document.getElementById("unordered").style.border= "2px solid black"; 
+
+    clearcontent(display);
 	
 } // End getData
 
+function clearcontent(display) {
+    display.innerHTML = "";
+}
+function showledinfo(){
+      ledinfo=document.getElementById("ledinfo");
+        ledinfo.style.visibility="visible";
+}
 
 
 
 function requestvader() {
 	let request = new XMLHttpRequest(); // Object för Ajax-anropet
 	request.open("GET"," https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json");//hämtar lat och lng i bildens id objekt
-	
-	console.log(request);
-
 	request.send(null); // Skicka begäran till servern
 	request.onreadystatechange = function () { // Funktion för att avläsa status i kommunikationen
 		if (request.readyState == 4)
